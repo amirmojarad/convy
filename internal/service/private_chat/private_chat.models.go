34a8f0971e2192c97b4c8f19a642ae07b750d5c2
amirmjrd <amir.mojarad@snapp.supply>
2023-11-07 19:18:33 +0330

    feat: add private chat service and service models templates

---
+++ internal/service/private_chat/private_chat.models.go
@@ -0,0 +1,40 @@
+package private_chat
+
+type Message struct {
+	SenderId      uint
+	ReceiverId    uint
+	PrivateChatId uint
+	Message       string
+}
+
+type CreateRequest struct {
+	// FirstUserId user that request create the chat
+	FirstUserId uint
+	// SecondUserId another user that participate in chat
+	SecondUserId uint
+}
+
+type CreateResponse struct {
+	Id uint
+}
+
+type AddMessageRequest struct {
+	PrivateChatId  uint
+	Message        string
+	SenderUserId   uint
+	ReceiverUserId uint
+}
+
+type AddMessageResponse struct {
+	Id      uint
+	Message string
+}
+
+type GetAllMessagesRequest struct {
+	PrivateChatId uint
+}
+
+type GetAllMessagesResponse struct {
+	Messages      []Message
+	PrivateChatId uint
+}
