1982113218a65aef4d55141e7f71eed44925ae0d
Amir Mojarad <49244395+amirmojarad@users.noreply.github.com>
2023-11-12 23:02:36 +0330

    rename database connector to postgres and drop private_chat_messages table (#12)
    * feat: add private_chat and private_chat_messages in migrations
    
    * feat: add private chat service and service models templates
    
    * feat: add private chat controller and repository layer
    
    * faet: add cors
    
    * feat: drop table private_chat_messages
    
    * chore: change database to postgres for more specifiction
    
    ---------
    
    Co-authored-by: amirmjrd <amir.mojarad@snapp.supply>

---
+++ database/migrations/20231112225342_drop_table_pr_chat_messages.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+
+drop table pr_chat_messages;
+
+-- +goose StatementBegin
+SELECT 'up SQL query';
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
